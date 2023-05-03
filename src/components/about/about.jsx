c1dda6da2b598258b54ce1a60818fb1c354471bd
Keerthan N S <keerthanns091@gmail.com>
2023-05-03 17:31:52 +0530

    [fix] about section

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,11 +1,27 @@
 import "./about.css";
 import Qualifications from "../qualifications/qualifications";
+import Buttons from '../button/button';
 
 function About() {
   return (
-    <div id="about" className="container about-container">
-      <Qualifications />
-    </div>
+    <>
+      <div id="about" className='h-screen container abt-container'>
+        <h2>
+          <span>About Me</span> <br />
+          <p className='indent-10 text-justify'>
+            I am currently pursuing BE in Computer Science and  I have a strong interest in web development.
+            I am working working on becoming a full-stack web developer. I find the process of creating websites and applications very satisfying and I enjoy the challenges that comes with it.
+          </p>
+          <p  className='indent-10 text-justify'>In my free time, I am crazy about gaming. It is a great way to unwind and relax after a long day. I enjoy playing games from various
+            genres, including Open-world like GTA5, strategy, racing games.
+          </p>
+        </h2>
+        <Buttons/>
+      </div>
+      <div className="container about-container">
+        <Qualifications />
+      </div>
+    </>
   );
 }
 export default About;
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,6 +1,5 @@
 import './home.css';
 import img from '../props/avatar.jpg';
-import Buttons from '../button/button';
 import { BsMouse } from 'react-icons/bs';
 import React,{useEffect} from 'react';
 
@@ -30,25 +29,12 @@ function Home() {
         <img src={img} alt='' />
       </div>
 
-      <a href='#footer' className='scroll-down'>
+      <a href='#about' className='scroll-down'>
         <hr />
         <h5>scroll down</h5>
         <BsMouse className='scroll' />
         <hr />
       </a>
-
-      <h2>
-        <span>About Me</span> <br />
-        <p className='indent-10 text-justify'>
-          I am currently pursuing BE in Computer Science and  I have a strong interest in web development.
-          I am working working on becoming a full-stack web developer. I find the process of creating websites and applications very satisfying and I enjoy the challenges that comes with it.
-        </p>
-        <p  className='indent-10 text-justify'>In my free time, I am crazy about gaming. It is a great way to unwind and relax after a long day. I enjoy playing games from various
-          genres, including Open-world like GTA5, strategy, racing games.
-        </p>
-      </h2>
-
-      <Buttons />
     </div>
   );
 }
