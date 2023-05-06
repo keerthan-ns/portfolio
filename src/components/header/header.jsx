45793baecaa59a3858ab46d3e5b7c0fe48ef231e
Keerthan N S <keerthanns091@gmail.com>
2023-05-06 18:41:26 +0530

    [add] typer animation

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,20 @@
 import './header.css';
+import { Typewriter } from "react-simple-typewriter"
 
 function Header() {
+  const status= ['Web Developer','Student']
   return (
     <div id='home' className='containerH header-container'>
       <h3>
+        <p>
+          Hello there<span>!</span>
+        </p>
         <p>
           I<span>'</span>m
         </p>
         Keerthan NS
       </h3>
-      <h2>Web Developer</h2>
+      <h2><Typewriter words={status} loop={0} cursor/></h2>
     </div>
   );
 }
