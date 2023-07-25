26219f342e78c54b90d6680e30fb314b5bbbe077
Keerthan N S <62982853+keerthan2002@users.noreply.github.com>
2023-07-25 23:16:03 +0530

    [update] resume

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -4,5 +4,6 @@ export const contact = {
     instagram: 'https://instagram.com/keerthan_ns',
     mail: 'mailto:keerthanns091@gmail.com',
     twitter:'https://twitter.com/keerthan_ns',
-    resume: 'https://drive.google.com/file/d/1hQe1MGIjXVx9CDUpBvn26NdxxKv3FlpD/view?usp=sharing'
+    resume: 'https://drive.google.com/file/d/1qmtZAYmzcZWpEPeMlOlTVcDHGT8Hjnoh/view?usp=sharing',
+    resumeold: 'https://drive.google.com/file/d/1hQe1MGIjXVx9CDUpBvn26NdxxKv3FlpD/view?usp=sharing'
   };
