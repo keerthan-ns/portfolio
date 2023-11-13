3ad3f127a0c647876145d143942205aa61712fdb
Keerthan N S <62982853+keerthan-ns@users.noreply.github.com>
2023-11-13 13:43:19 +0530

    [update] links & resume

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,9 +1,10 @@
 export const contact = {
-    github: 'https://github.com/keerthan2002',
+    github: 'https://github.com/keerthan-ns',
     linkedin: 'https://www.linkedin.com/in/keerthan-n-s-220142208/',
     instagram: 'https://instagram.com/keerthan_ns',
     mail: 'mailto:keerthanns091@gmail.com',
     twitter:'https://twitter.com/keerthan_ns',
-    resume: 'https://drive.google.com/file/d/1BOgUCeZmaC_FQrx8LrwiYe3198UgV7G5/view?usp=sharing',
+    resume: 'https://drive.google.com/file/d/1VLhuCzWlPN_Qe-W9hnxFn97BlgD3Tb3i/view?usp=sharing',
+    // resume: 'https://drive.google.com/file/d/1PxDi5pwZtVZ5JHE6YUOEOwrOP2ZvSFMn/view?usp=sharing',
     resumeold: 'https://drive.google.com/file/d/1hQe1MGIjXVx9CDUpBvn26NdxxKv3FlpD/view?usp=sharing'
   };
