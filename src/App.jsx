2f7f6c5705199e04672aa96a35748b37f2c510f0
Keerthan N S <keerthanns091@gmail.com>
2025-03-06 06:59:28 +0530

    [feat] 3D bee on scroll using gsap [add] 3D bee [update] loader visibility until the bee is loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useEffect, useState } from "react"
+import { Fragment } from "react"
 import Loader from './Loaders/Loader'
 import Header from "./components/header/header"
 import Home from "./components/home/home"
@@ -12,50 +12,50 @@ import Navbar from "./components/nav/nav"
 import { Analytics } from '@vercel/analytics/react'
 
 function App() {
-  const [loading , setLoading] = useState(true)
+  // const [loading , setLoading] = useState(true)
 
-  useEffect(() => {
-    setLoading(true)
-    setTimeout(() => {
-      setLoading(false);
-    }, 5000);
+  // useEffect(() => {
+  //   setLoading(true)
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 5000);
     
-  }, [])
+  // }, [])
 
-  if(loading){
-    return(<>
-      <Loader/>
-    </>)
-  }
+  // if(loading){
+  //   return(<>
+  //     <Loader/>
+  //   </>)
+  // }
 
   return (
-      <>
-        <Analytics/>
-        <div className="bg-circle1"></div>
-        <div className="bg-circle2"></div>
-        <section id='homeSect'>
-          <div id="home" className='h-screen '>
-            {/* <img src={'https://res.cloudinary.com/dg7etzwks/image/upload/v1683643876/portfolioProjects/k0e6d6uhxc3aety2saca.jpg'} className='h-screen absolute w-full self-center object-cover' alt='background'/>         */}
-            <img src={'https://res.cloudinary.com/dg7etzwks/image/upload/v1683852442/portfolioProjects/cegavi7l1m0grdabdbhn.jpg'} className='h-screen absolute w-full self-center object-cover' alt='background'/>        
-            <Header/>
-            <Home />
-          </div>
-        </section>
-        <section id='aboutSect'>
-          <About />
-        </section>
-        <section id='techSect'>
-          <Techs />
-        </section>
-        <section id='projectSect'>
-          <Project/>
-        </section>
-        <section id='contactSect'>
-          <Contact />
-        </section>
-          <Footer />
-        <Navbar/>
-      </>
+    <Fragment>
+      <Loader/>
+      <Analytics/>
+      <div className="bg-circle1"></div>
+      <div className="bg-circle2"></div>
+      <section className='section' id='homeSect'>
+        <div id="home" className='h-screen '>
+          <img src={'https://res.cloudinary.com/dg7etzwks/image/upload/v1683852442/portfolioProjects/cegavi7l1m0grdabdbhn.jpg'} className='h-screen absolute w-full self-center object-cover' alt='background'/>        
+          <Header/>
+          <Home />
+        </div>
+      </section>
+      <section className='section' id='aboutSect'>
+        <About />
+      </section>
+      <section className='section' id='techSect'>
+        <Techs />
+      </section>
+      <section className='section' id='projectSect'>
+        <Project/>
+      </section>
+      <section className='section' id='contactSect'>
+        <Contact />
+      </section>
+        <Footer />
+      <Navbar/>
+    </Fragment>
   );
 }
 
--- src/Loaders/Loader.js
+++ src/Loaders/Loader.js
@@ -3,14 +3,14 @@ import "./Loader.css"
 
 function Loader() {
   return (
-    <>
+    <div className='loader-bg' id='loader-bg'>
         <div className="lcontainer">
             <div className="ring"></div>
             <div className="ring"></div>
             <div className="ring"></div>
         </div>
         <div className="glitch " data-text="LOADING...">LOADING...</div>
-    </>
+    </div>
   )
 }
 
